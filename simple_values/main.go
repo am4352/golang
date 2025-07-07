6bfb77408d3e74b053a83eb901e9b423be298ff8
Anuj Mishra <158813198+am4352@users.noreply.github.com>
2025-07-07 20:56:36 +0530

    studied loops and range

---
+++ simple_values/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt" // println uses fmt package 
+
+func main() {
+	fmt.Println(1+23)
+	//var foo string = "Go is awesome";
+	var foo, bar string = "hello " , "world";
+	print(foo)
+	print(bar);
+}
